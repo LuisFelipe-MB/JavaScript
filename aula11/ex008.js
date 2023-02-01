377a52960ea75b59a8c22db8233940584b254e3c
LuisFelipe-MB <109697962+LuisFelipe-MB@users.noreply.github.com>
2023-02-01 12:10:56 -0300

    novo exercício

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 86
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velosidade permitida!. MULTADO!')
+}
+console.log('Dirija com cuidado e use o sinto de segurança!')
