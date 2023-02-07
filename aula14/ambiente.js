7a1158df57f519f82f8022bf8169d0f156dd67c2
LuisFelipe-MB <109697962+LuisFelipe-MB@users.noreply.github.com>
2023-02-07 10:56:02 -0300

    novo exercício

---
+++ aula14/ambiente.js
@@ -0,0 +1,35 @@
+/*console.log("VAI COMEÇAR...")
+for (var c=1; c<=4; c++) {
+    console.log(c)
+}
+console.log("FIM!")
+*/
+
+//Fibonacc------
+var a1 = 1
+var a2 = 1
+var a3 = 0
+console.log(a1)
+console.log(a2)
+for (var c = 3; c<=13; c++) {
+    a3 = a1 + a2
+    console.log(a3)
+    a1 = a2
+    a2 = a3
+}
+
+
+/*
+var c = 1
+while (c <=10) {
+    console.log(`Passo ${c}`)
+    c++ 
+}
+*/
+//---------------
+
+/*
+for(var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+*/
