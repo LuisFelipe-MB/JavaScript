aa72bad0b89caa10d8daa3f4033705c76d309b5a
LuisFelipe-MB <109697962+LuisFelipe-MB@users.noreply.github.com>
2023-02-09 13:02:25 -0300

    materia

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [4,7,3,9,0]
+num.push(2)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`o primeiro elemento do vetor é ${num[0]}`) 
+for(let i=0; i < num.length; i++) {
+    console.log(`A posição ${i} tem o valor ${num[i]}`)
+}
