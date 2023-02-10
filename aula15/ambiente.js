636d65da8f508eb11c82450577e41592c90e71f6
LuisFelipe-MB <109697962+LuisFelipe-MB@users.noreply.github.com>
2023-02-10 10:38:24 -0300

    terminei o exercício

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,9 +1,14 @@
 let num = [4,7,3,9,0]
-num.push(2)
+/*num.push(2)
 num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`o primeiro elemento do vetor é ${num[0]}`) 
 for(let i=0; i < num.length; i++) {
     console.log(`A posição ${i} tem o valor ${num[i]}`)
+}
+*/
+
+for(let pos in num) {
+    console.log(`A passição ${pos} tem o valor ${num[pos]}`)
 }
---
+++ aula15/vetorambiente.js
@@ -0,0 +1,3 @@
+let num = [9,3,6,1,0,2]
+let pos = num.indexOf(9)
+console.log(`O vetor 8 está na posição ${pos}`)
